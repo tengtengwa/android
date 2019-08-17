91fc94caba856438ce446012f699350bb4fc17f3
tengtengwa <49139650+tengtengwa@users.noreply.github.com>
2019-08-17 19:28:57 +0800

    Add files via upload
    第一行代码

---
+++ UIPractice/main/java/com/example/uipractice/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.uipractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Msg> msgList = new ArrayList<>();
+    private EditText inputText;
+    private Button send;
+    private RecyclerView msgRecyclerView;
+    private MsgAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initMsgs();
+        inputText = findViewById(R.id.input_text);
+        send = findViewById(R.id.send);
+        msgRecyclerView = findViewById(R.id.msg_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecyclerView.setLayoutManager(layoutManager);
+        adapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String content = inputText.getText().toString();
+                if(!"".equals(content)) {
+                    Msg msg = new Msg(content, Msg.TYPE_SENT);
+                    msgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size() - 1);
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1);
+                    inputText.setText("");
+
+                }
+            }
+        });
+
+    }
+
+    private void initMsgs() {
+        Msg msg1 = new Msg("你好，伙计~", Msg.TYPE_RECEIVED);
+        msgList.add(msg1);
+        Msg msg2 = new Msg("你好，你是哪个笨猪~", Msg.TYPE_SENT);
+        msgList.add(msg2);
+        Msg msg3 = new Msg("我是川建国同志，很高兴见到你~", Msg.TYPE_RECEIVED);
+        msgList.add(msg3);
+
+
+    }
+
+}
